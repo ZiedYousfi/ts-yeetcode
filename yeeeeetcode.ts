b225a8727f4ac75fa24e5cbe66594fbf1e430b2e
ZiedYousfi <yousfi_zied@yahoo.com>
2025-06-26 16:52:09 +0200

    Add Merge Two Sorted Lists function implementation

--- yeeeeetcode.ts
+++ yeeeeetcode.ts
@@ -138,3 +138,147 @@ function romanToInt(s: string): number {
   }
   return result;
 }
+
+// 21. Merge Two Sorted Lists
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  // Attempt #1
+
+  // if(!list1 && !list2){
+  //   return null;
+  // }
+
+  // let result: ListNode | null = new ListNode();
+
+  // if (list1 && !list2) {
+  //   result.val = list1.val;
+  //   return result;
+  // }
+
+  // if (list2 && !list1) {
+  //   result.val = list2.val;
+  //   return result;
+  // }
+
+  // if(!list1 || !list2){
+  //   return null;
+  // }
+
+  // let next: ListNode | null = new ListNode();
+
+  // result.next = next;
+
+  // if (list1?.val < list2?.val) {
+  //   result.val = list1?.val;
+  //   next.val = list2?.val;
+  // } else if (list1.val === list2.val) {
+  //   result.val = list1.val;
+  //   next.val = list2.val;
+  // } else {
+  //   result.val = list2?.val;
+  //   next.val = list1?.val;
+  // }
+
+  // next.next = mergeTwoLists(list1.next, list2.next);
+
+  // // sort the result list
+  // let current: ListNode | null = result;
+  // let sorted: ListNode | null = null;
+
+  // while (current) {
+  //   let next = current.next;
+  //   if (next) {
+  //     if (next.val > current.val) {
+  //     const temp = current.val;
+  //     current.val = next.val;
+  //     next.val = temp;
+  //     }
+  //   }
+  //   current = next;
+  // }
+
+  // return result;
+
+  // Attempt #2
+
+  if (!list1 && !list2) {
+    return null;
+  }
+
+  interface ListToArray {
+    (list: ListNode | null): number[];
+  }
+
+  const list1_as_array: ListToArray = (list1: ListNode | null): number[] => {
+    let array: number[] = [];
+    let current: ListNode | null = list1;
+    let next: ListNode | null = list1 ? list1.next : null;
+    while (current) {
+      if (current !== null && current.val !== undefined) {
+        array.push(current.val);
+      }
+      current = next;
+      next =  next ? next.next : null;
+    }
+    return array;
+  }
+
+  const list2_as_array: ListToArray = (list2: ListNode | null): number[] => {
+    let array: number[] = [];
+    let current: ListNode | null = list2;
+    let next: ListNode | null = list2 ? list2.next : null;
+    while (current) {
+      if (current !== null && current.val !== undefined) {
+        array.push(current.val);
+      }
+      current = next;
+
+      next =  next ? next.next : null;
+    }
+    return array;
+  }
+
+  const final_array = list1_as_array(list1).concat(list2_as_array(list2));
+  final_array.sort((n1,n2) => n1 - n2);
+
+  if (final_array.length === 0) {
+    return null;
+  }
+
+  let result: ListNode | null = new ListNode;
+  let current = result;
+
+  for (const val of final_array) {
+    current.next = new ListNode(val);
+    current = current.next;
+  }
+
+  return result.next;
+}
